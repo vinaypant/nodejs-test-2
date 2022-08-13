e8bf8ee059519c9aadbec9ed8fb6ed5bb276559f
Vinay Pant <vinay.pant@taazaa.com>
2022-08-13 17:21:15 +0530

    chalk module

---
+++ 6-chalk-module.mjs
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+console.log(chalk.blue("hello wprld"))
+console.log(chalk.blue.italic("hello wprld"))
+console.log(chalk.blue.underline("hello wprld"))
+console.log(chalk.blue.inverse("hello wprld"))
+
+console.log(chalk.green.underline.inverse("success"))
+console.log(chalk.red.underline.inverse("fail"))
